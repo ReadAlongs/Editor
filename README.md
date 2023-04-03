36a194e277854e5f90593e89d44a60f198f6ce8e
David Huggins-Daines <dhd@ecolingui.ca>
2023-04-03 14:25:31 -0400

    chore: initial import

---
+++ README.md
@@ -0,0 +1,10 @@
+# ReadAlong Alignment Editor
+
+This is a quite simple little web application that will allow the
+alignments in a ReadAlong (either a standalone HTML file, or an XML
+file + associated audio, or some combination of the above) to be
+edited.
+
+For the moment it does not quite work, nor does the UI behave as you'd
+expect, but at least it allows you to look at the alignments!
+x
---
+++ src/index.ts
@@ -0,0 +1,72 @@
+import WaveSurfer from "wavesurfer.js";
+import RegionsPlugin from "wavesurfer.js/src/plugin/regions";
+
+// Will be removed by webpack
+require("purecss");
+require("./index.css");
+
+class App {
+  wavesurfer: WaveSurfer;
+  audio_input: HTMLInputElement;
+  ras_input: HTMLInputElement;
+
+  constructor() {
+    this.audio_input = document.getElementById(
+      "audio-input"
+    ) as HTMLInputElement;
+    this.ras_input = document.getElementById("ras-input") as HTMLInputElement;
+    this.wavesurfer = WaveSurfer.create({
+      container: "#wavesurfer",
+      plugins: [RegionsPlugin.create({})],
+      scrollParent: true,
+      height: 200,
+      minPxPerSec: 300, // FIXME: uncertain about this
+    });
+  }
+
+  load_audiofile(audio_file: File) {
+    this.wavesurfer.loadBlob(audio_file);
+  }
+
+  async load_readalong(ras_file: File) {
+    const parser = new DOMParser();
+    const text = await ras_file.text();
+    const xml = parser.parseFromString(text, "text/html");
+    for (const w of Array.from(xml.querySelectorAll("read-along w[id]"))) {
+      const wordText = w.textContent;
+      const startText = w.getAttribute("time");
+      const durText = w.getAttribute("dur");
+      if (wordText == null || startText == null || durText == null) continue;
+      const startTime = parseFloat(startText);
+      const endTime = startTime + parseFloat(durText);
+      this.wavesurfer.addRegion({
+        attributes: { label: wordText.trim() },
+        start: startTime,
+        end: endTime,
+      });
+    }
+  }
+
+  initialize() {
+    this.audio_input.addEventListener("change", () => {
+      if (this.audio_input.files != null)
+        this.load_audiofile(this.audio_input.files[0]);
+    });
+    this.ras_input.addEventListener("change", async () => {
+      if (this.ras_input.files != null)
+        this.load_readalong(this.ras_input.files[0]);
+    });
+    this.wavesurfer.on("region-click", (region, e) => {
+      console.log(
+        `${region.attributes.label}: ${region.start} -> ${region.end}`
+      );
+      e.stopPropagation();
+      region.play();
+    });
+  }
+}
+
+window.addEventListener("load", () => {
+  const app = new App();
+  app.initialize();
+});
