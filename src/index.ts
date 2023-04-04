c02c221c7bbf7cf717fcc8e1b0ddd55c94731289
David Huggins-Daines <dhd@ecolingui.ca>
2023-04-03 22:41:24 -0400

    chore: prettify everything with 4 spaces

--- src/index.ts
+++ src/index.ts
@@ -6,82 +6,89 @@ require("purecss");
 require("./index.css");
 
 class App {
-  wavesurfer: WaveSurfer;
-  audio_input: HTMLInputElement;
-  ras_input: HTMLInputElement;
-  zoom_in: HTMLButtonElement;
-  zoom_out: HTMLButtonElement;
+    wavesurfer: WaveSurfer;
+    audio_input: HTMLInputElement;
+    ras_input: HTMLInputElement;
+    zoom_in: HTMLButtonElement;
+    zoom_out: HTMLButtonElement;
 
-  constructor() {
-    this.audio_input = document.getElementById(
-      "audio-input"
-    ) as HTMLInputElement;
-    this.ras_input = document.getElementById("ras-input") as HTMLInputElement;
-    this.zoom_in = document.getElementById("zoom-in") as HTMLButtonElement;
-    this.zoom_out = document.getElementById("zoom-out") as HTMLButtonElement;
-    this.wavesurfer = WaveSurfer.create({
-      container: "#wavesurfer",
-      plugins: [
-        RegionsPlugin.create({
-          contentEditable: true,
-          removeButton: true,
-          formatTimeCallback: (start: number, end: number) =>
-            `${start.toFixed(2)}:${end.toFixed(2)}`,
-        }),
-      ],
-      scrollParent: true,
-      height: 200,
-      minPxPerSec: 300, // FIXME: uncertain about this
-    });
-  }
-
-  load_audiofile(audio_file: File) {
-    this.wavesurfer.loadBlob(audio_file);
-  }
-
-  async load_readalong(ras_file: File) {
-    const parser = new DOMParser();
-    const text = await ras_file.text();
-    const xml = parser.parseFromString(text, "text/html");
-    for (const w of Array.from(xml.querySelectorAll("read-along w[id]"))) {
-      const wordText = w.textContent;
-      const startText = w.getAttribute("time");
-      const durText = w.getAttribute("dur");
-      if (wordText == null || startText == null || durText == null) continue;
-      const startTime = parseFloat(startText);
-      const endTime = startTime + parseFloat(durText);
-      this.wavesurfer.addRegion({
-        data: { text: wordText.trim() },
-        start: startTime,
-        end: endTime,
-      });
+    constructor() {
+        this.audio_input = document.getElementById(
+            "audio-input"
+        ) as HTMLInputElement;
+        this.ras_input = document.getElementById(
+            "ras-input"
+        ) as HTMLInputElement;
+        this.zoom_in = document.getElementById("zoom-in") as HTMLButtonElement;
+        this.zoom_out = document.getElementById(
+            "zoom-out"
+        ) as HTMLButtonElement;
+        this.wavesurfer = WaveSurfer.create({
+            container: "#wavesurfer",
+            plugins: [
+                RegionsPlugin.create({
+                    contentEditable: true,
+                    removeButton: true,
+                    formatTimeCallback: (start: number, end: number) =>
+                        `${start.toFixed(2)}:${end.toFixed(2)}`,
+                }),
+            ],
+            scrollParent: true,
+            height: 200,
+            minPxPerSec: 300, // FIXME: uncertain about this
+        });
     }
-  }
 
-  initialize() {
-    this.audio_input.addEventListener("change", () => {
-      if (this.audio_input.files != null)
-        this.load_audiofile(this.audio_input.files[0]);
-    });
-    this.ras_input.addEventListener("change", async () => {
-      if (this.ras_input.files != null)
-        this.load_readalong(this.ras_input.files[0]);
-    });
-    this.zoom_in.addEventListener("click", () => {
-      this.wavesurfer.zoom(this.wavesurfer.params.minPxPerSec * 1.25);
-    });
-    this.zoom_out.addEventListener("click", () => {
-      this.wavesurfer.zoom(this.wavesurfer.params.minPxPerSec / 1.25);
-    });
-    this.wavesurfer.on("region-click", (region, e) => {
-      console.log(`${region.data.text}: ${region.start} -> ${region.end}`);
-      e.stopPropagation();
-      region.play();
-    });
-  }
+    load_audiofile(audio_file: File) {
+        this.wavesurfer.loadBlob(audio_file);
+    }
+
+    async load_readalong(ras_file: File) {
+        const parser = new DOMParser();
+        const text = await ras_file.text();
+        const xml = parser.parseFromString(text, "text/html");
+        for (const w of Array.from(xml.querySelectorAll("read-along w[id]"))) {
+            const wordText = w.textContent;
+            const startText = w.getAttribute("time");
+            const durText = w.getAttribute("dur");
+            if (wordText == null || startText == null || durText == null)
+                continue;
+            const startTime = parseFloat(startText);
+            const endTime = startTime + parseFloat(durText);
+            this.wavesurfer.addRegion({
+                data: { text: wordText.trim() },
+                start: startTime,
+                end: endTime,
+            });
+        }
+    }
+
+    initialize() {
+        this.audio_input.addEventListener("change", () => {
+            if (this.audio_input.files != null)
+                this.load_audiofile(this.audio_input.files[0]);
+        });
+        this.ras_input.addEventListener("change", async () => {
+            if (this.ras_input.files != null)
+                this.load_readalong(this.ras_input.files[0]);
+        });
+        this.zoom_in.addEventListener("click", () => {
+            this.wavesurfer.zoom(this.wavesurfer.params.minPxPerSec * 1.25);
+        });
+        this.zoom_out.addEventListener("click", () => {
+            this.wavesurfer.zoom(this.wavesurfer.params.minPxPerSec / 1.25);
+        });
+        this.wavesurfer.on("region-click", (region, e) => {
+            console.log(
+                `${region.data.text}: ${region.start} -> ${region.end}`
+            );
+            e.stopPropagation();
+            region.play();
+        });
+    }
 }
 
 window.addEventListener("load", () => {
-  const app = new App();
-  app.initialize();
+    const app = new App();
+    app.initialize();
 });
--- webpack.config.cjs
+++ webpack.config.cjs
@@ -6,53 +6,53 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const isProduction = process.env.NODE_ENV == "production";
 
 const config = {
-  entry: "./src/index.ts",
-  output: {
-    clean: true,
-  },
-  devServer: {
-    open: true,
-    host: "localhost",
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "src/index.html",
-    }),
-    new MiniCssExtractPlugin(),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
-        type: "asset",
-      },
-      // Add your rules for custom modules here
-      // Learn more about loaders from https://webpack.js.org/loaders/
-      {
-        test: /\.css$/i,
-        use: [MiniCssExtractPlugin.loader, "css-loader"],
-      },
-      {
-        test: /\.tsx?$/,
-        use: "ts-loader",
-        exclude: /node_modules/,
-      },
+    entry: "./src/index.ts",
+    output: {
+        clean: true,
+    },
+    devServer: {
+        open: true,
+        host: "localhost",
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "src/index.html",
+        }),
+        new MiniCssExtractPlugin(),
     ],
-  },
-  resolve: {
-    extensions: [".ts", ".js"],
-  },
-  performance: {
-    maxAssetSize: 3500000,
-    maxEntrypointSize: 300000,
-  },
+    module: {
+        rules: [
+            {
+                test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+                type: "asset",
+            },
+            // Add your rules for custom modules here
+            // Learn more about loaders from https://webpack.js.org/loaders/
+            {
+                test: /\.css$/i,
+                use: [MiniCssExtractPlugin.loader, "css-loader"],
+            },
+            {
+                test: /\.tsx?$/,
+                use: "ts-loader",
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    resolve: {
+        extensions: [".ts", ".js"],
+    },
+    performance: {
+        maxAssetSize: 3500000,
+        maxEntrypointSize: 300000,
+    },
 };
 
 module.exports = () => {
-  if (isProduction) {
-    config.mode = "production";
-  } else {
-    config.mode = "development";
-  }
-  return config;
+    if (isProduction) {
+        config.mode = "production";
+    } else {
+        config.mode = "development";
+    }
+    return config;
 };
